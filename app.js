63ec342b91cd52f8d8f9e33d2035fc62864e58b0
Jordan-Queiroz <jordan.s.queiroz@gmail.com>
2022-07-18 19:19:20 -0400

    basic handlers for mybooks endpoint

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('My Books')
+})
+
+app.get('/mybooks', (req, res) => {
+    res.send('List books')
+})
+
+app.post('/mybooks', (req, res) => {
+    res.send('Add books')
+})
+
+app.put('/mybooks', (req, res) => {
+    res.send('Update books')
+})
+
+app.delete('/mybooks', (req, res) => {
+    res.send('Delete books')
 })
 
 app.listen(port, () => {
